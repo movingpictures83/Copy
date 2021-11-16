6df489c85983e50f380b4fc5847ae07f6be1ac29
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2021-11-16 15:49:42 -0500

    Use system copy, not line-by-line

--- CopyPlugin.py
+++ CopyPlugin.py
@@ -1,19 +1,21 @@
 import PyPluMA
+import os
 
 class CopyPlugin:
    def input(self, filename):
-      #self.myfile = filename
-      txtfile = open(filename, 'r')
-      self.lines = []
-      for line in txtfile:
-         self.lines.append(line)
+      self.myfile = filename
+      #txtfile = open(filename, 'r')
+      #self.lines = []
+      #for line in txtfile:
+      #   self.lines.append(line)
 
    def run(self):
       pass
 
    def output(self, filename):
-      filestuff2 = open(filename, 'w')
-      for line in self.lines:
-          filestuff2.write(line)
+      os.system("cp "+self.myfile+" "+filename)
+      #filestuff2 = open(filename, 'w')
+      #for line in self.lines:
+      #    filestuff2.write(line)
 
 
